bb3f677da04b3e47e5163a09e23f033298effb76
Pavel Tkachenko <tpepost@gmail.com>
2022-09-24 16:26:10 +0600

    Construction started 🚧

---
+++ spec/kassa24_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Kassa24 do
+  it "has a version number" do
+    expect(Kassa24::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
